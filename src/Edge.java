13d0334c7943f4ce6771568ef7e05ac7903a13bd
Fr3ddaz <follinenjohansson@gmail.com>
2014-11-21 11:54:05 +0100

    first commit

---
+++ src/Edge.java
@@ -0,0 +1,29 @@
+/**
+ * @author Fredrik Ollinen Johansson
+ * @version 2014-11-21
+ */
+
+public class Edge {
+
+	private final int start;
+	private final int end;
+	private final int cost;
+	
+	public Edge (int start, int end, int cost) {
+		this.start = start;
+		this.end = end;
+		this.cost = cost;
+	}
+	
+	public int getStart() {
+		return start;
+	}
+	
+	public int getEnd() {
+		return end;
+	}
+	
+	public int getCost() {
+		return cost;
+	}
+}
---
+++ src/SolutionState.java
@@ -0,0 +1,7 @@
+/**
+ * @author Christine Solnon
+ *
+ */
+public enum SolutionState {
+	    NO_SOLUTION_FOUND, INCONSISTENT, OPTIMAL_SOLUTION_FOUND, SOLUTION_FOUND 
+}
