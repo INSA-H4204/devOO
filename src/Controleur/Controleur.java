81034eb77e350b4ec8945eb05ff40f062cd84874
hubergerard <huber.gerard1@gmail.com>
2014-11-28 11:48:04 +0100

    Selection des noeuds retravaillée. Insérer point de livraison en cours de codage

--- src/Controleur/Controleur.java
+++ src/Controleur/Controleur.java
@@ -15,11 +15,23 @@ import Vue.VueZone;
  * @author hgerard
  */
 public class Controleur {
+	
 	public VueZone vueNoeud;
 	public VueZone vueTroncon;
 	private Zone zone;
 	private boolean isZoneSansLivraison;
+	
+	// Noeud sélectionné à l'ajout
 	private Noeud noeudSelectionne;
+	
+	// Noeud sélectionné après l'appui du bouton Ajouter
+	private Noeud noeudPrecedent;
+	
+	// Quand ce booléen est faux, l'utilisateur ne peut pas cliquer sur le plan
+	private boolean selectionActive;
+	
+	// Vrai quand on a cliqué sur Ajouter
+	private boolean ajoutEnCours;	
 
 	/**
 	 * Constructeur de la classe Controleur
@@ -28,10 +40,16 @@ public class Controleur {
 	 * @author hgerard
 	 */
 	public Controleur(Zone zone) {
-		this.zone = zone;
 		
+		this.zone = zone;
 		vueNoeud = new VueNoeud(this);
 		vueTroncon = new VueTroncon(this);
+
+		isZoneSansLivraison = true;
+		ajoutEnCours = true;
+		selectionActive = true;
+		noeudSelectionne = null;
+		noeudPrecedent = null;
 	}
 
 	/**
@@ -46,11 +64,26 @@ public class Controleur {
 	 * Sélectionne un noeud
 	 * @param 	int x 				La coordonée x du click effectué sur la carte
 	 * @param 	int y 				La coordonée y du click effectué sur la carte
-	 * @return	Noeud noeudClique 	Le noeud qui a été cliqué
 	 */
 	private void selectionnerNoeud(int x, int y){
-		Noeud noeudClique = zone.rechercherNoeudParPosition(x,y);
-		this.noeudSelectionne = noeudClique;
+		
+		if (selectionActive) {
+			selectionActive = false;
+			Noeud noeudClique = zone.rechercherNoeudParPosition(x,y);
+			if (noeudClique != null) {
+				if (ajoutEnCours){
+					this.noeudPrecedent = noeudClique;
+				} else {
+					this.noeudSelectionne = noeudClique;
+				}
+				if (noeudSelectionne.getLivraison() == null) {
+					// Vue.ActiverBoutonAjouter -> Gabriel
+				} else {
+					// Vue.ActiverBoutonSupprimer -> Gabriel
+				}
+			}
+			selectionActive = true;
+		}
 	}
 
 	/**
