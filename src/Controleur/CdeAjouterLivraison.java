2216cf4107e075a626f689226c710d81861816b0
thomas-helmer <thomas.helmer69@gmail.com>
2014-12-03 00:59:51 +0100

    fonction ajouterLivraison

--- src/Controleur/CdeAjouterLivraison.java
+++ src/Controleur/CdeAjouterLivraison.java
@@ -7,6 +7,7 @@ import Modele.Livraison;
 import Modele.Noeud;
 import Modele.PlageHoraire;
 import Modele.Tournee;
+import Modele.Troncon;
 import Modele.Zone;
 
 /**
@@ -44,7 +45,7 @@ public class CdeAjouterLivraison extends Commande {
 		this.livraisonPrecedente = livraisonPrecedente;
 		
 		Livraison livraisonAjout = noeudSelectionne.getLivraison();
-		this.livraisonPrecedente = livraisonPrecedente;
+		this.livraisonAjout = livraisonAjout;
 		
 		PlageHoraire plageAjout = livraisonPrecedente.getPlage();
 		this.plageAjout = plageAjout;
@@ -52,10 +53,29 @@ public class CdeAjouterLivraison extends Commande {
 
 	/**
 	 * Fonction appelée quand on execute la fonction normalement
+	 * @author thelmer
 	 */
 
 	protected void execute() {
+		int posCheminSupprimer=-2;
 		List<Chemin> chemins = zone.getTournee().getChemins();
+		for(Chemin chemin:chemins){
+			if(posCheminSupprimer != -2){
+				int adressePrecedente= livraisonPrecedente.getAdresse().getNoeudID();
+				int adresseAjoute = livraisonAjout.getAdresse().getNoeudID();
+				int adresseSuivante = chemin.getArrivee().getAdresse().getNoeudID();
+				Chemin cheminPrecedent = zone.plusCourtChemin(adressePrecedente,adresseAjoute);
+				Chemin cheminSuivant = zone.plusCourtChemin(adresseAjoute,adresseSuivante);
+				zone.getTournee().getChemins().remove(posCheminSupprimer);
+				zone.getTournee().getChemins().add(posCheminSupprimer,cheminPrecedent);
+				zone.getTournee().getChemins().add(posCheminSupprimer+1,cheminSuivant);
+				return;
+			}
+			else{
+				if(chemin.getArrivee() == livraisonPrecedente)
+					posCheminSupprimer = chemins.indexOf(chemin)+1;
+			}
+		}
 
 		
 	}
